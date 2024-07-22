4f40dd98b61813292d24a22296680233799bcc89
Brenda Ombeyi <brendaotieno2001@gmail.com>
2024-07-22 13:46:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+// Write your code 
+let num1=31 
+let num2 =2
+let multiply = (num1 * num2)
+//random
+let random = Math.floor(Math.random()*10)+1
+//mod
+let mod = num3 % num4 
+let num3 = 400
+let num4 = 6
+//max
 
-// Write your code here
+//let max = Math.max(a,b,c)
+let a = 2
+let b =20
+let c = 10
+let max = Math.max(2,10,20)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
